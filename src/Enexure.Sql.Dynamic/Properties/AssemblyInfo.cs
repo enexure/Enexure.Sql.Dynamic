374209e79480d681dc07410ecd8252f7b54ea54e
Daniel Little <lavinski@live.com.au>
2015-01-18 17:45:18 +1000

    Add versioning

--- src/Enexure.Sql.Dynamic/Properties/AssemblyInfo.cs
+++ src/Enexure.Sql.Dynamic/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f4062faa-9e40-4c8d-9811-80f6d0ba6428")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
