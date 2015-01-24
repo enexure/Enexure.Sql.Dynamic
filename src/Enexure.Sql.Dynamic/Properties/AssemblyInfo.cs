a6dbc4ca8ab14da1a47ba18720697a0955342435
Daniel Little <Lavinski@live.com.au>
2015-01-24 12:54:01 +1000

    Added assembly description

--- src/Enexure.Sql.Dynamic/Properties/AssemblyInfo.cs
+++ src/Enexure.Sql.Dynamic/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Enexure.Sql.Dynamic")]
-[assembly: AssemblyDescription("Daniel Little")]
+[assembly: AssemblyDescription("Immutable dynamic SQL generation library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Daniel Little")]
 [assembly: AssemblyProduct("Enexure.Sql.Dynamic")]
